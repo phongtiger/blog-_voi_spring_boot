c726e17842612704b5e0c7813d84fb064e2ae288
phongtiger <phongken94@gmail.com>
2019-10-16 21:02:55 +0700

    da phan quyen mac dinh da chay ngon. them file application

---
+++ src/main/java/com/example/taoblogdongian/TaoblogdongianApplication.java
@@ -0,0 +1,13 @@
+package com.example.taoblogdongian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaoblogdongianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaoblogdongianApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/taoblogdongian/WebController.java
@@ -0,0 +1,17 @@
+package com.example.taoblogdongian;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+    @GetMapping(value = {"/", "/home"})
+    public String homepage() {
+        return "home"; // Trả về home.html
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello"; // Trả về hello.html
+    }
+}
